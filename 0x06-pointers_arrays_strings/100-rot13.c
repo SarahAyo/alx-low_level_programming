129d6106b2f3e0c760bc642367f577f48054d5e0
SarahAyo <sarahayobami@gmail.com>
2022-10-27 15:06:54 -0700

    taskall

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,22 +7,22 @@
 
 char *rot13(char *s)
 {
-        int i, j;
+	int i, j;
 
-        char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                for (j = 0; x[j] != '\0'; j++)
-                {
-                        if (s[i] == x[j])
-                        {
-                                s[i] = y[j];
-                                break;
-                        }
-                }
-        }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; x[j] != '\0'; j++)
+		{
+			if (s[i] == x[j])
+			{
+				s[i] = y[j];
+				break;
+			}
+		}
+	}
 
-        return (s);
+	return (s);
 }
